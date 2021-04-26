c08e293e6e60d82b174028f4c6a5095ccaad014a
Elekrisk <einar.vilhelm.persson@gmail.com>
2021-04-26 18:31:22 +0200

    Extracted initialization to separate function
    Extracted initialization of modules from main to it's own function.
    Calls to initialize after the first one currently silently does nothing,
    but it may be preferable to instead print an error message
    or to panic.

--- src/main.rs
+++ src/main.rs
@@ -14,28 +14,52 @@ mod ps2;
 mod ps2_keyboard;
 mod gdt;
 
-use core::panic::PanicInfo;
+use core::{panic::PanicInfo, sync::atomic::{AtomicBool, Ordering}};
 
 use bootloader::BootInfo;
-use gdt::initialize;
 
 #[no_mangle]
 pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
-    // Safety: this is safe
-    unsafe {
-        gdt::initialize();
-        idt::initialize();
+    // No function call may precede this one, or else undefined behaviour may be invoked.
+    initialize(boot_info);
 
-        printer::initialize(core::ptr::read(boot_info.framebuffer.as_ref().unwrap()));
-        printer::clear();
-
-        pic::initialize();
-        x86_64::instructions::interrupts::enable();
-        ps2::initialize();
-    }
     println!("Hello, World!");
 
     loop {}
+}
+
+/// Initializes all modules.
+///
+/// Must be the first function called in `_start`.
+fn initialize(boot_info: &BootInfo) {
+    static INITIALIZED: AtomicBool = AtomicBool::new(false);
+
+    // Atomics can be quite confusing.
+    // This call to `compare_exchange` compares the value of `INITIALIZED`, and if it is `false`, we write `true` to it.
+    // This happens atomically, so there is no risk of data races.
+    // When the comparison succeeds, `compare_exchange` returns an Ok(_), which means that `INITIALIZED` was `false` and now is `true`,
+    // ergo we haven't yet initialized, and should do so.
+    // If it returns an `Err(_)`, the comparison failed, so `INITIALIZED` is `true`, and we should not try to initialize again.
+    if INITIALIZED.compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed).is_ok() {
+
+        // Safety: All modules' initialization functions have orderings that must be guaranteed
+        // to avoid undefined behaviour. These are currently undocumented,
+        // but the orderings are respected in the below block.
+        unsafe {
+            gdt::initialize();
+            idt::initialize();
+    
+            // The call to `ptr::read` is safe here, as a reference is always valid for reads,
+            // and as `Framebuffer` has no custom destructor and is only comprised of
+            // integers and structs of integers. (and an enum with #[repr(C)])
+            printer::initialize(core::ptr::read(boot_info.framebuffer.as_ref().unwrap()));
+            printer::clear();
+            
+            pic::initialize();
+            x86_64::instructions::interrupts::enable();
+            ps2::initialize();
+        }
+    }
 }
 
 #[panic_handler]
