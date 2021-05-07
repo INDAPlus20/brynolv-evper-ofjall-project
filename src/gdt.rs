abba073ed31352e7e34e8ac16ae01ea270bc1c25
brynolv <72003588+brynolv@users.noreply.github.com>
2021-05-07 20:13:19 +0200

    Added rustfmt config and enforced it with GitHub Action (#44)
    * Added Formating action and rustfmt config
    
    Non-default fmt config options marked with comments.
    Fixes #43

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,6 +1,5 @@
 use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable};
 
-
 static mut GDT: GlobalDescriptorTable = GlobalDescriptorTable::new();
 
 /// Initializes the Global Descriptor Table.
@@ -9,9 +8,9 @@ static mut GDT: GlobalDescriptorTable = GlobalDescriptorTable::new();
 ///
 /// This should not be called if another call to this function has not yet returned.
 pub unsafe fn initialize() {
-    let code_segment = GDT.add_entry(Descriptor::kernel_code_segment());
-    let data_segment = GDT.add_entry(Descriptor::kernel_data_segment());
-    GDT.load();
-    x86_64::instructions::segmentation::load_ss(data_segment);
-    x86_64::instructions::segmentation::set_cs(code_segment);
+	let code_segment = GDT.add_entry(Descriptor::kernel_code_segment());
+	let data_segment = GDT.add_entry(Descriptor::kernel_data_segment());
+	GDT.load();
+	x86_64::instructions::segmentation::load_ss(data_segment);
+	x86_64::instructions::segmentation::set_cs(code_segment);
 }
--- src/svec.rs
+++ src/svec.rs
@@ -1,112 +1,117 @@
-use core::{mem::MaybeUninit, ops::{Index, IndexMut}};
-
-
+use core::{
+	mem::MaybeUninit,
+	ops::{Index, IndexMut},
+};
 
 pub struct SVec<T, const N: usize> {
-    inner: [MaybeUninit<T>; N],
-    length: usize,
+	inner: [MaybeUninit<T>; N],
+	length: usize,
 }
 
 impl<T, const N: usize> SVec<T, N> {
-    pub const fn new() -> Self {
-        Self {
-            inner: MaybeUninit::uninit_array(),
-            length: 0,
-        }
-    }
+	pub const fn new() -> Self {
+		Self {
+			inner: MaybeUninit::uninit_array(),
+			length: 0,
+		}
+	}
 }
 
 impl<T, const N: usize> SVec<T, N> {
-    pub fn len(&self) -> usize {
-        self.length
-    }
+	pub fn len(&self) -> usize {
+		self.length
+	}
 
-    pub fn capacity(&self) -> usize {
-        N
-    }
+	pub fn capacity(&self) -> usize {
+		N
+	}
 
-    pub fn push(&mut self, value: T) {
-        self.inner[self.length] = MaybeUninit::new(value);
-        self.length += 1;
-    }
+	pub fn push(&mut self, value: T) {
+		self.inner[self.length] = MaybeUninit::new(value);
+		self.length += 1;
+	}
 
-    pub fn pop(&mut self) -> Option<T> {
-        if self.length > 0 {
-            self.length -= 1;
-            Some(unsafe { self.inner[self.length].assume_init_read() })
-        } else {
-            None
-        }
-    }
+	pub fn pop(&mut self) -> Option<T> {
+		if self.length > 0 {
+			self.length -= 1;
+			Some(unsafe { self.inner[self.length].assume_init_read() })
+		} else {
+			None
+		}
+	}
 
-    pub fn remove(&mut self, index: usize) -> T {
-        if index >= self.length {
-            panic!("Index out of bounds");
-        }
+	pub fn remove(&mut self, index: usize) -> T {
+		if index >= self.length {
+			panic!("Index out of bounds");
+		}
 
-        unsafe {
-            let t = core::ptr::read(&self.inner[index]).assume_init();
-            if index + 1 < self.length {
-                core::ptr::copy(&self.inner[index + 1], &mut self.inner[index], self.length - index - 1);
-            }
-            self.length -= 1;
-            t
-        }
-    }
+		unsafe {
+			let t = core::ptr::read(&self.inner[index]).assume_init();
+			if index + 1 < self.length {
+				core::ptr::copy(
+					&self.inner[index + 1],
+					&mut self.inner[index],
+					self.length - index - 1,
+				);
+			}
+			self.length -= 1;
+			t
+		}
+	}
 
-    pub fn get_slice(&self) -> &[T] {
-        unsafe { core::mem::transmute(&self.inner[..self.length]) }
-    }
+	pub fn get_slice(&self) -> &[T] {
+		unsafe { core::mem::transmute(&self.inner[..self.length]) }
+	}
 
-    pub fn get_slice_mut(&mut self) -> &mut [T] {
-        unsafe { core::mem::transmute(&mut self.inner[..self.length]) }
-    }
+	pub fn get_slice_mut(&mut self) -> &mut [T] {
+		unsafe { core::mem::transmute(&mut self.inner[..self.length]) }
+	}
 }
 
 impl<T, const N: usize> Index<usize> for SVec<T, N> {
-    type Output = T;
+	type Output = T;
 
-    fn index(&self, index: usize) -> &Self::Output {
-        if index >= self.length {
-            panic!(
-                "Index out of bounds; index was {}, max was {}",
-                index,
-                self.length - 1
-            );
-        } else {
-            unsafe { self.inner[index].assume_init_ref() }
-        }
-    }
+	fn index(&self, index: usize) -> &Self::Output {
+		if index >= self.length {
+			panic!(
+				"Index out of bounds; index was {}, max was {}",
+				index,
+				self.length - 1
+			);
+		} else {
+			unsafe { self.inner[index].assume_init_ref() }
+		}
+	}
 }
 
 impl<T, const N: usize> IndexMut<usize> for SVec<T, N> {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        if index >= self.length {
-            panic!(
-                "Index out of bounds; index was {}, max was {}",
-                index,
-                self.length - 1
-            );
-        } else {
-            unsafe { self.inner[index].assume_init_mut() }
-        }
-    }
+	fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+		if index >= self.length {
+			panic!(
+				"Index out of bounds; index was {}, max was {}",
+				index,
+				self.length - 1
+			);
+		} else {
+			unsafe { self.inner[index].assume_init_mut() }
+		}
+	}
 }
 
 impl<T: Clone, const N: usize> Clone for SVec<T, N> {
-    fn clone(&self) -> Self {
-        let mut ret = SVec::new();
-        for i in self.get_slice() {
-            ret.push(i.clone());
-        }
-        ret
-    }
+	fn clone(&self) -> Self {
+		let mut ret = SVec::new();
+		for i in self.get_slice() {
+			ret.push(i.clone());
+		}
+		ret
+	}
 }
 
 impl<T, const N: usize> Drop for SVec<T, N> {
-    fn drop(&mut self) {
-        for item in self.get_slice_mut() {
-            core::mem::drop(item);
-        }
-    }
+	fn drop(&mut self) {
+		for item in self.get_slice_mut() {
+			core::mem::drop(item);
+		}
+	}
 }
