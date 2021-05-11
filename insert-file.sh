10102ef57bccf24ba459f1cea4ea275ba90faf9a
Elekrisk <einar.vilhelm.persson@gmail.com>
2021-05-11 18:06:55 +0200

    Added script for adding the file test.txt to the FAT root
    Running `build.py` will overwrite the FAT file,
    so don't do that after running `insert-file.sh`.
    
    Co-authored-by: brynolv <k.jzfgxgh@gmail.com>
    Co-authored-by: Vikdos <viktor.ofjall@gmail.com>

---
+++ insert-file.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mcopy -i out/boot-uefi-brynolv-evper-ofjall-project.fat test.txt ::test.txt
+dd if=out/boot-uefi-brynolv-evper-ofjall-project.fat \
+of=out/boot-uefi-brynolv-evper-ofjall-project.img bs=1K count=1K \
+conv=notrunc seek=17
